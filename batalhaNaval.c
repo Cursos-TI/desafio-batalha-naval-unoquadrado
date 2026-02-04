ffcf47521b37f242a52ceed2353cd5ae02884e48
unoquadrado <meugitgitgitgit@gmail.com>
2026-02-04 17:52:59 +0000

    tabuleiro preenchido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,46 @@
-#include <stdio.h>
+#include <stdio.h> 
+  int main() {
 
-int main() 
-{
-  //tabuleiro 10x10
-  int tabuleiro[10][10];
+    char coluna[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int i, j;
+    int tabuleiro[10][10];
 
-  for(int i = 0; i < 10; i++) {
-    for(int j = 0; j < 10; j++) {
-      tabuleiro[i][j] = 0;
+        for(i = 0; i < 10; i++) {
+            for(j = 0; j < 10; j++) {
+                tabuleiro[i][j] = 0;
+        }
     }
-  }
+        
+    //preenchendo lugares no tabuleiro com loop for
+        for(i = 3; i <= 5; i++) {
+            tabuleiro[2][i] = 3;
+        }      
+        
+       for(j = 5; j <= 7; j++) {
+            tabuleiro[j][7] = 3;
+       }
+        
+        printf("TABULEIRO BATALHA NAVAL\n");
 
-  printf("TABULEIRO BATALHA NAVAL:\n");
-  for(int i = 0; i < 10; i++) {
-    for(int j = 0; j < 10; j++) {
-      printf("%d ", tabuleiro[i][j]);
-    }
-    printf("\n");
-  }
+        printf("    ");
+        for(i = 0; i < 10; i++) {
+            printf(" %c ", coluna[i]);
+            
+        } printf("\n");
 
-  return 0; 
-}
+        for(i = 0; i < 10; i++) {
+            printf(" %2d ", i + 1);
+                for(j = 0; j < 10; j++) {
+                    printf(" %d ", tabuleiro[i][j]);
+            } printf("\n");
+
+        }
+
+            
+            
+        
+
+
+
+        return 0;
+  }
